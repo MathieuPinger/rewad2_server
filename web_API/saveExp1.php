b64d3a76a6732c507f18d3f40ca793683a116b23
MathieuPinger <mathieu.pinger@zi-mannheim.de>
2021-01-19 17:25:25 +0100

    paths...

--- web_API/saveExp1.php
+++ web_API/saveExp1.php
@@ -9,6 +9,6 @@ $name = "../data/".$id."_exp1.csv";
 file_put_contents($name, $save_data);
 // sleep(10);
 file_put_contents("log.txt", "test");
-$pythonstuff = shell_exec("/usr/lib/python-virtualenvs/rewad2/rewad2_server/bin/python /var/www/clox/rewad2/rewad2_server/web_api/input_output_B08optimize.py $id");
+$pythonstuff = shell_exec("/usr/lib/python-virtualenvs/rewad/bin/python /var/www/clox/rewad2/rewad2_server/web_api/input_output_B08optimize.py $id");
 echo $pythonstuff;
 ?>
